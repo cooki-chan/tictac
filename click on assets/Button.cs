2822659919364362f4c2970f55b2609399be9027
Ethan Nguyen <ethan41717@gmail.com>
2023-09-28 10:50:10 -0400

    e

--- click on assets/Button.cs
+++ click on assets/Button.cs
@@ -5,7 +5,7 @@ public class Button : Godot.Button
 {
     // Declare member variables here. Examples:
     // private int a = 2;
-    // private string b = "text";
+    private bool test = false;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
