f16ec2c92cce44499358001bea2f30ead50eac6c
Ethan Nguyen <ethan41717@gmail.com>
2023-10-06 10:46:44 -0400

    ig i do know what im doing lol

--- netwirk/MultiplayerController.cs
+++ netwirk/MultiplayerController.cs
@@ -9,15 +9,21 @@ public class MultiplayerController : Control
     // private string b = "text";
 
     // Called when the node enters the scene tree for the first time.
+    private Label label;
+    private Label msgLbl;
+    private TextEdit inp;
 
     NetworkedMultiplayerENet peer = new NetworkedMultiplayerENet();
     public override void _Ready()
     {
+        label = GetNode<Label>("Label");
+        msgLbl = GetNode<Label>("msgLbl");
+        inp = GetNode<TextEdit>("TextEdit");
     }
-
-    public void _on_host_button_down(){
+    public void _on_host_button_down(){  
         peer.CreateServer(2222, 1);
         GetTree().NetworkPeer = peer;
+        label.Text = "server "+ GetTree().GetNetworkUniqueId();
         Debug.Print("Servr is Server?: " + GetTree().IsNetworkServer());
         GetTree().Connect("network_peer_connected", this, "_player_connected");
         GetTree().Connect("network_peer_disconnected", this, "_player_disconnected");
@@ -29,6 +35,7 @@ public class MultiplayerController : Control
     public void _on_join_button_down(){
         peer.CreateClient("127.0.0.1", 2222);
         GetTree().NetworkPeer = peer;
+        label.Text = "client " + GetTree().GetNetworkUniqueId();
         Debug.Print("Client is Server?: " + GetTree().IsNetworkServer());
         GetTree().Connect("network_peer_connected", this, "_player_connected");
         GetTree().Connect("network_peer_disconnected", this, "_player_disconnected");
@@ -38,16 +45,25 @@ public class MultiplayerController : Control
     }
 
     public void _on_start_button_down(){
-        throw new NotImplementedException();
+        Rpc("greetings", inp.Text);
     }
 
     String[] name = {"Johnson Magenta"};
     void _player_connected(int id){
-        RpcId(id, "hello_world", name);
         Debug.Print("Connect: " + id);
     }
 
     void _player_disconnected(int id){
         Debug.Print("Disconnect: " + id);
+    }
+
+    void _on_disconnect_button_down(){
+        GetTree().NetworkPeer = null;
+        label.Text = " none";
+    }
+
+    [Remote]
+    void greetings(String msg){
+        msgLbl.Text += "\n" + msg;
     }
 }
