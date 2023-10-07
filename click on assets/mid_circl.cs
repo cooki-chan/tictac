0fc400256d6fb77d6ba96e3bbd9459d0b65696bf
Bananaz21 <72627687+cooki-chan@users.noreply.github.com>
2023-10-07 00:26:41 -0400

    ee

--- click on assets/mid_circl.cs
+++ click on assets/mid_circl.cs
@@ -4,15 +4,15 @@ using System.Diagnostics;
 
 public class mid_circl : Sprite
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
@@ -20,18 +20,18 @@ public class mid_circl : Sprite
 //      
 //  }
 
-    private Boolean vis = false;
+	private Boolean vis = false;
 
-    public void _on_Button_pressed(){
-        GD.Print("circ: clicked");
-        if(vis){
-            Visible = !Visible;
-        }
-    }
+	public void _on_Button_pressed(){
+		GD.Print("circ: clicked");
+		if(vis){
+			Visible = !Visible;
+		}
+	}
 
-    public void _on_XO_Button_toggled(bool visibility){
-        GD.Print("circ: switched");
-        vis = visibility;
-        Visible = false;
-    }
+	public void _on_XO_Button_toggled(bool visibility){
+		GD.Print("circ: switched");
+		vis = visibility;
+		Visible = false;
+	}
 }
