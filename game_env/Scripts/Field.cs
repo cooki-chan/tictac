4497b2135feee89a3ead54bbe1c03c9206e4d1ae
Morkith <105373735+Morkith@users.noreply.github.com>
2024-02-16 11:11:11 -0500

    IT WON"T ******* WORK (SparseMatrix attachment)

---
+++ game_env/Scripts/Field.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class Field : ColorRect{
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
